888c07cce86478bf3ec63ca5c9b760a482be46e4
0tavioPascoal <fjoooosa@gmail.com>
2026-01-13 23:39:54 -0300

    feat: learning routes, methods, url, status code

--- src/server.js
+++ src/server.js
@@ -1,8 +1,30 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
-  return res.end('Hello')
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res
+    .writeHead(200)
+    .setHeader('Content-type', 'application/json')
+    .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: users.length + 1,
+      name: 'John Doe',
+      email: 'johnDoe@example.com'
+    })
+
+    return res.writeHead(201).end()
+  }
+
+  return res.writeHead(404).end()
 })
 
-
-server.listen(3333)
+server.listen(3333, () => {
+  console.log('Server running on http://localhost:3333')
+})
